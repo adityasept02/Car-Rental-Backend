04da154e38bf50f902cbc58fbcaf7d2b0e02c198
adityasept02 <adityasept02@gmail.com>
2024-11-16 23:20:45 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,3 @@
+export const DATABASE = "mongodb+srv://adityamaurya:aditya@buysell.3nbcfw8.mongodb.net/?retryWrites=true&w=majority&appName=buysell";
+export const JWT_SECRET = "FHJR2323RFWDFSFDS32S";
+export const FIREBASE_API_KEY = "AIzaSyCdkG6khMtPztesRm1ayY2Qtw3XLj4mEzI";
